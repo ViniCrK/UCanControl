9f272d5de5387e9016b97de437f0585d98b0cef6
ViniCrK <vinicrk@outlook.com>
2024-05-20 09:29:04 -0300

    Add alert messages to be shown on the screen

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -8,36 +8,76 @@ import styles from "./styles";
 
 export default function Main() {
   const [distancia, setDistancia] = useState(0);
+  const [statusSensor, setStatusSensor] = useState(false);
 
   useEffect(() => {
     const fetchDistancia = async () => {
       const distRef = ref(db, "distancia");
       onValue(distRef, (snapshot) => {
-        const data = snapshot.val();
-        setDistancia(data);
+        const dist = snapshot.val();
+        setDistancia(dist);
+      });
+    };
+
+    const fetchStatusSensor = async () => {
+      const statusSensorRef = ref(db, "statusSensor");
+      onValue(statusSensorRef, (snapshot) => {
+        const status = snapshot.val();
+        setStatusSensor(status);
       });
     };
 
     fetchDistancia();
+    fetchStatusSensor();
   }, []);
 
-  const changeBoxBgColor = () => {
+  const distanceMessages = {
+    danger: "Precisa ser removido",
+    warning: "Precisa de atenção",
+    safe: "Não precisa remover",
+  };
+
+  function showDistanceMessage() {
+    if (distancia <= 5) {
+      return distanceMessages.danger;
+    } else if (distancia <= 15) {
+      return distanceMessages.warning;
+    }
+    return distanceMessages.safe;
+  }
+
+  function changeDistanceBgColor() {
     if (distancia <= 5) {
       return styles.distanceBoxDanger;
     } else if (distancia <= 15) {
       return styles.distanceBoxWarning;
     }
     return styles.distanceBoxSafe;
-  };
+  }
+
+  const rfidSensorMessage = "💳 Passe o cartão no sensor!";
+
+  function showRfidSensorMessage() {
+    if (statusSensor == true) {
+      return rfidSensorMessage;
+    }
+    return;
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.context}>
-        <Text style={styles.subtitle}>Lixeiras:</Text>
-        <Text style={styles.labelText}>1ª Lixeira</Text>
-        <Text style={[styles.distanceBox, changeBoxBgColor()]}>
-          {distancia}cm
+        <Text style={styles.subtitle}>Distâncias:</Text>
+        <View style={styles.row}>
+          <Text style={styles.labelText}>Lixeira:</Text>
+          <Text style={[styles.distanceBox, changeDistanceBgColor()]}>
+            {distancia}cm
+          </Text>
+        </View>
+        <Text style={[styles.alertText, changeDistanceBgColor()]}>
+          🗑️ {showDistanceMessage()}
         </Text>
+        <Text style={[styles.alertText]}>{showRfidSensorMessage()}</Text>
       </View>
     </View>
   );

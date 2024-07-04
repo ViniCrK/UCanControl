50dad1f9b9174477d1e9dcd80ce47c66e4acb0dc
ViniCrK <vinicrk@outlook.com>
2024-07-04 17:45:31 -0300

    Add alerts | Remove the section of distance messages from RFID

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,11 +1,57 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, Alert } from "react-native";
 
 import { db } from "../../services/firebase";
 import { ref, onValue } from "firebase/database";
 
 import styles from "./styles";
 import { ILixeira } from "../../models/Lixeira";
+
+const showAlert = (lixeiraId: number) => {
+  Alert.alert("Remoção de lixo", `Retire o lixo da Lixeira ${lixeiraId}!`, [
+    {
+      text: "Não há lixo?",
+      onPress: () =>
+        Alert.alert(
+          "Desculpe pelo incômodo.",
+          "Ainda estamos em fase de testes."
+        ),
+    },
+    {
+      text: "Cancelar",
+      onPress: () => console.log("Requisição da remoção: NEGADA"),
+    },
+    {
+      text: "OK",
+      onPress: () => console.log("Requisição da remoção: ACEITA"),
+    },
+  ]);
+};
+
+const distanceMessages = {
+  danger: "Precisa ser removido",
+  warning: "Precisa de atenção",
+  safe: "Não precisa remover",
+};
+
+function showDistanceMessage(lixeiraID: number, distancia: number) {
+  if (distancia <= 5) {
+    showAlert(lixeiraID);
+    return distanceMessages.danger;
+  } else if (distancia <= 15) {
+    return distanceMessages.warning;
+  }
+  return distanceMessages.safe;
+}
+
+function changeDistanceBgColor(distancia: number) {
+  if (distancia <= 5) {
+    return styles.distanceBoxDanger;
+  } else if (distancia <= 15) {
+    return styles.distanceBoxWarning;
+  }
+  return styles.distanceBoxSafe;
+}
 
 export default function Main() {
   const [lixeiras, setLixeiras] = useState<ILixeira[]>([]);
@@ -26,39 +72,6 @@ export default function Main() {
 
     fetchLixeiras();
   }, []);
-
-  const distanceMessages = {
-    danger: "Precisa ser removido",
-    warning: "Precisa de atenção",
-    safe: "Não precisa remover",
-  };
-
-  function showDistanceMessage(distancia: number) {
-    if (distancia <= 5) {
-      return distanceMessages.danger;
-    } else if (distancia <= 15) {
-      return distanceMessages.warning;
-    }
-    return distanceMessages.safe;
-  }
-
-  function changeDistanceBgColor(distancia: number) {
-    if (distancia <= 5) {
-      return styles.distanceBoxDanger;
-    } else if (distancia <= 15) {
-      return styles.distanceBoxWarning;
-    }
-    return styles.distanceBoxSafe;
-  }
-
-  function showRfidSensorMessage(status: boolean) {
-    const rfidSensorMessage = "💳 Passe o cartão no sensor!";
-
-    if (status == false) {
-      return rfidSensorMessage;
-    }
-    return;
-  }
 
   return (
     <View style={styles.container}>
@@ -83,11 +96,11 @@ export default function Main() {
                 changeDistanceBgColor(lixeira.distancia),
               ]}
             >
-              🗑️ {showDistanceMessage(lixeira.distancia)}
+              🗑️ {showDistanceMessage(lixeira.id, lixeira.distancia)}
             </Text>
-            <Text style={[styles.alertText]}>
+            {/* <Text style={[styles.alertText]}>
               {showRfidSensorMessage(lixeira.foiTrocado)}
-            </Text>
+            </Text> */}
           </View>
         ))}
       </View>
